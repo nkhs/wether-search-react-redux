c099c6d7b1cf7d16ff12f5f883c190702503dad5
unknown <nkhs@yandex.com>
2021-02-09 20:34:32 +0800

    updated

--- src/components/app.js
+++ src/components/app.js
@@ -10,7 +10,16 @@ export default class App extends Component {
     return (
       <div>
         <SearchBar />
-        <WeatherList/>
+        <WeatherList />
+        <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-5 space-y-6 h-screen bg-red-500">
+          <div class="flex-shrink-023">
+            <img class="h-12 w-12 rounded-2xl" src="/sarah-dayan.jpg" alt="ChitChat Logo" />
+          </div>
+          <div>
+            <div class="text-xl font-medium text-black">ChitChat</div>
+            <p class="text-gray-500">You have a new message!</p>
+          </div>
+        </div>
       </div>
     )
   }
--- src/components/chat.js
+++ src/components/chat.js
@@ -6,12 +6,12 @@ function average(data) {
 }
 export default (props) => {
     return (
-        <>
+        <div className="flex flex-col justify-start content-start">
             <Sparklines data={props.data} width={100} height={50} margin={5}>
                 <SparklinesLine color={props.color} />
                 <SparklinesReferenceLine />
             </Sparklines>
             <div>{average(props.data)} {props.unit}</div>
-        </>
+        </div>
     )
 }
